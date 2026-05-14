a64e631d6da057d4bbfe2bf153deabab1d479f61
ashikahmedfahim <ashikahmed96@gmail.com>
2026-05-14 17:23:45 +0200

    feat: add global error handling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { type NextFunction, type Request, type Response } from 'express';
 import bodyParser from 'body-parser';
 import 'dotenv/config';
 import { SQLDatabase } from './database/index.js';
@@ -12,11 +12,22 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(Logger.getHttpLogger());
 
-app.use('/health', (req, res) => {
+app.use('/health', (_req, res) => {
   res.status(200).json({ status: 'ok' });
 });
 
 app.use('/v1/users', userRoute);
+
+app.use((err: Error | { statusCode?: number; message?: string }, _req: Request, res: Response, _next: NextFunction) => {
+  const { statusCode = 500, message = 'Internal Server Error' } = err as { statusCode?: number; message?: string };
+  const logMessage = `Request: ${_req.method} ${_req.url}, Error: ${message}`;
+  if (statusCode >= 500) {
+    Logger.error(logMessage, err);
+  } else {
+    Logger.warn(logMessage, err);
+  }
+  res.status(statusCode).json({ message });
+});
 
 SQLDatabase.getInstance().$connect();
 
