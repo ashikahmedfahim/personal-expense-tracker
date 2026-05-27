9985ed6ae6242acf649641eca2ecec58f9959721
ashikahmedfahim <ashikahmed96@gmail.com>
2026-05-27 19:59:29 +0200

    feat: add categoty model to db

---
+++ src/prisma/migrations/20260527175909_add_category/migration.sql
@@ -0,0 +1,13 @@
+-- CreateEnum
+CREATE TYPE "FlowType" AS ENUM ('INFLOW', 'OUTFLOW');
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "flowType" "FlowType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
